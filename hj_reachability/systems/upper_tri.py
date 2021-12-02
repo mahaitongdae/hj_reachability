96df9cb4c2af8132648f6c7ad375b8b9f225b646
MaHaitong <mahaitongdae@foxmail.com>
2021-12-02 16:34:13 +0800

    add double int

--- hj_reachability/systems/upper_tri.py
+++ hj_reachability/systems/upper_tri.py
@@ -1,1 +1,39 @@
-upper_triangle
+import jax.numpy as jnp
+
+from hj_reachability import dynamics
+from hj_reachability import sets
+
+class DetAir3d(dynamics.ControlAndDisturbanceAffineDynamics):
+
+    def __init__(self,
+                 range=5.,
+                 max_u = 0.5,
+                 control_mode="max",
+                 disturbance_mode="min",
+                 control_space=None,
+                 disturbance_space=None):
+        self.evader_speed = evader_speed
+        self.pursuer_speed = pursuer_speed
+        if control_space is None:
+            control_space = sets.Box(lo=jnp.array([-max_u]), hi=jnp.array([max_u]))
+        if disturbance_space is None:
+            disturbance_space = sets.Box(lo=jnp.array([-max_u]), hi=jnp.array([max_u]))
+        super().__init__(control_mode, disturbance_mode, control_space, disturbance_space)
+
+    def open_loop_dynamics(self, state, time):
+        x_1, x_2 = state
+        # v_a, v_b = self.evader_speed, self.pursuer_speed
+        return jnp.array([x_2, 0])
+
+    def control_jacobian(self, state, time):
+        x, y, _ = state
+        return jnp.array([
+            [0],
+            [1],
+        ])
+
+    def disturbance_jacobian(self, state, time):
+        return jnp.array([
+            [0.],
+            [0.],
+        ])
